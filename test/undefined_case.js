8abbd1e8a13d4c3ef6b94e68c407c28f0383c727
fffuture <t30t40t50@gmail.com>
2017-01-04 02:50:12 +0200

    ruby edit

--- test/undefined_case.js
+++
@@ -1,9 +0,0 @@
-const Letov = require('../lib/index')();
-
-for (let i = 0; i < 10000; i += 1) {
-	if (Letov.random() === undefined) {
-		throw 'Андефайнд найден.' + i
-	} 
-}
-
-console.log('Тест пройден.')
